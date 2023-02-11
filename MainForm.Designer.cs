24834b5792414261ff71fe6911e0aedad202bb0b
Ethan_Vodka <gohho1008@gmail.com>
2023-02-12 00:48:33 +0900

    プロジェクト ファイルを追加します。

---
+++ MainForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Sample3D
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer.IsSplitterFixed = true;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.BtnStart);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.pictureBox);
+            this.splitContainer.Size = new System.Drawing.Size(423, 443);
+            this.splitContainer.SplitterDistance = 34;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(337, 3);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(75, 23);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 443);
+            this.Controls.Add(this.splitContainer);
+            this.Name = "MainForm";
+            this.Text = "3DRotate";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample3D
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private struct Pos
+        {
+            public float x;
+            public float y;
+            public float z;
+        }
+
+        //中心座標
+        private static readonly PointF Center = new PointF(200, 200);
+
+        //オリジナル座標
+        private static Pos[] op =
+        {
+            //手前
+            new Pos()
+            {
+                x = 100,
+                y = 100,
+                z = 0,
+            }
+            , new Pos()
+             {
+                x=100,
+                y=300,
+                z=0,
+            }
+            , new Pos()
+            {
+                x = 300,
+                y = 300,
+                z = 0,
+            }
+            , new Pos()
+            {
+                x = 300,
+                y = 100,
+                z = 0,
+            }
+            //奥
+            , new Pos()
+            {
+                x = 150,
+                y = 150,
+                z = 0,
+            }
+            , new Pos()
+             {
+                x=150,
+                y=250,
+                z=0,
+            }
+            , new Pos()
+            {
+                x = 250,
+                y = 250,
+                z = 0,
+            }
+            , new Pos()
+            {
+                x = 250,
+                y = 150,
+                z = 0,
+            }
+        };
+
+
+
+        static float Deg { get; set; } = 0;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Draw3D()
+        {
+            Bitmap objBmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics objGrp = Graphics.FromImage(objBmp);
+            Pen objPen = new Pen(Color.Red, 2.0f);
+
+
+            //変動座標定義
+            Pos[] p = new Pos[8];
+
+            float theta = Deg / 180;
+            float s = (float)Math.Sin(theta);
+            float c = (float)Math.Cos(theta);
+
+
+            for (int i = 0; i < op.Length; i++)
+            {
+                p[i].x = op[i].x * c + op[i].y * -s;
+                p[i].y = op[i].y;
+                p[i].z = op[i].x * s + op[i].z * c;
+
+                p[i].z += 2;
+
+                ////z座標により奥行見せる
+                p[i].x /= p[i].z;
+                p[i].y /= p[i].z;
+            }
+
+            Deg += 5;
+            for (int i = 0; i < op.Length / 2; i++)
+            {
+                int j = (i + 1) % 4;
+                objGrp.DrawLine(objPen, p[i].x + Center.X, p[i].y + Center.Y, p[j].x + Center.X, p[j].y + Center.Y);
+                objGrp.DrawLine(objPen, p[i + 4].x + Center.X, p[i + 4].y + Center.Y, p[j + 4].x + Center.X, p[j + 4].y + Center.Y);
+                objGrp.DrawLine(objPen, p[i].x + Center.X, p[i].y + Center.Y, p[i + 4].x + Center.X, p[i + 4].y + Center.Y);
+            }
+            pictureBox.Image = objBmp;
+
+            //ガベージ処理
+            objGrp.Dispose();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            //Draw3D();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw3D();
+        }
+    }
+}
